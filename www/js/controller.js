5c0d39db673958953cb63cf9ec61ec54343aad3b
denzildoyle <yell@denzildoyle.me>
2015-03-25 10:42:05 -0400

    Uncomment sms share function and change sms share and function name to provide some consistancy

--- www/js/controller.js
+++ www/js/controller.js
@@ -1,26 +1,26 @@
 angular.module('starter.controllers', [])
 
 .controller('homeCtrl', function($scope, $cordovaSocialSharing) {
-    $scope.sendSMS = function (){
-        // var number = "18687967004";
-        // var message = "This is a test sms";
-
-        // //CONFIGURATION
-        // var options = {
-        //     replaceLineBreaks: false, // true to replace \n by a new line, false by default
-        //     android: {
-        //         intent: 'INTENT'  // send SMS with the native android SMS messaging
-        //         //intent: '' // send SMS without open any other app
-        //     }
-        // };
-
-        // var success = function () { alert('Message sent successfully'); };
-        // var error = function (e) { alert('Message Failed:' + e); };
-        // sms.send(number, message, options, success, error);
-    }
-
     $scope.shareAnywhere = function() {
         $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://blog.nraboy.com");
+    }
+
+    $scope.shareViaSMS = function (){
+        var number = "18687967004";
+        var message = "This is a test sms";
+
+        //CONFIGURATION
+        var options = {
+            replaceLineBreaks: false, // true to replace \n by a new line, false by default
+            android: {
+                intent: 'INTENT'  // send SMS with the native android SMS messaging
+                //intent: '' // send SMS without open any other app
+            }
+        };
+
+        var success = function () { alert('Message sent successfully'); };
+        var error = function (e) { alert('Message Failed:' + e); };
+        sms.send(number, message, options, success, error);
     }
 
     $scope.shareViaTwitter = function(message, image, link) {
