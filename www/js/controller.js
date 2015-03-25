92dd19470f9033c0e6ee0417f06f78db83f62481
denzildoyle <yell@denzildoyle.me>
2015-03-25 12:04:45 -0400

    Add facebook share

--- www/js/controller.js
+++ www/js/controller.js
@@ -31,4 +31,14 @@ angular.module('starter.controllers', [])
             alert("Cannot share on Twitter");
         });
     }
+
+    $scope.shareViaFacebook = function(message, image, link) {
+        $cordovaSocialSharing
+        .shareViaFacebook(message, image, link)
+        .then(function(result) {
+          // Success!
+        }, function(err) {
+          // An error occurred. Show a message to the user
+        });
+    }
 });
