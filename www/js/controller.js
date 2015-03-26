e3941cdfb1147f6fcffb010f5f99fcd668b57f8b
denzildoyle <yell@denzildoyle.me>
2015-03-26 09:29:48 -0400

    Add Icon

--- www/js/controller.js
+++ www/js/controller.js
@@ -5,22 +5,14 @@ angular.module('starter.controllers', [])
         $cordovaSocialSharing.share("This is your message", "This is your subject", "www/imagefile.png", "http://www.denzildoyle.me");
     }
 
-    $scope.shareViaSMS = function (){
-        var number = "18687967004";
-        var message = "Test sms!";
-
-        //CONFIGURATION
-        var options = {
-            replaceLineBreaks: false, // true to replace \n by a new line, false by default
-            android: {
-                intent: 'INTENT'  // send SMS with the native android SMS messaging
-                //intent: '' // send SMS without open any other app
-            }
-        };
-
-        var success = function () { alert('Message sent successfully'); };
-        var error = function (e) { alert('Message Failed:' + e); };
-        sms.send(number, message, options, success, error);
+    $scope.shareViaSMS = function (message, number){
+        $cordovaSocialSharing
+        .shareViaSMS(message, number)
+        .then(function(result) {
+          // Success!
+        }, function(err) {
+            alert("Cannot send SMS");
+        });
     }
 
     $scope.shareViaTwitter = function(message, image, link) {
@@ -38,7 +30,7 @@ angular.module('starter.controllers', [])
         .then(function(result) {
           // Success!
         }, function(err) {
-          // An error occurred. Show a message to the user
+            alert("Cannot share on Facebook");
         });
     }
 });
